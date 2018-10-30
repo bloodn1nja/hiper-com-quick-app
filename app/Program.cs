29d111745d822666f2e29b6406e1d7649636275b
Alex <realllizardking@gmail.com>
2018-10-30 16:16:11 +0100

    Update Program.cs

--- app/Program.cs
+++ app/Program.cs
@@ -22,7 +22,7 @@ namespace quick_app
                 item.Add("price:" + (prod.SelectSingleNode(".//aside[contains(@class, 'prices')]/p[contains(@class, 'standard')]/span[contains(@class, 'p-nb')]").InnerText +
                     "." + prod.SelectSingleNode(".//aside[@class='prices']/p[@class='standard']/span[@class='p-cents']").InnerText));
                 item.Add("packaging:" + prod.SelectSingleNode(".//div[@class = 'description'] / p[@class = 'packaging']/strong").InnerText);
-                item.Add("imagePath:" + prod.SelectSingleNode("..//div[@class='picture']/img").GetAttributeValue("data-src", "nothing"));
+                item.Add("imagePath:" + prod.SelectSingleNode(".//div[@class='picture']/img").GetAttributeValue("data-src", "nothing"));
             }
         }
     }
